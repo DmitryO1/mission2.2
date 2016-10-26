d6ce01c533fc75f5d9f5c8765e098b216b493b7d
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-10-26 15:11:25 +0300

    misson2.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 import com.sun.deploy.util.StringUtils;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 import java.util.Scanner;
 
@@ -9,23 +14,16 @@ class untitled3 {
 
     public static void main(String[] args) {
 
+            Scanner in = new Scanner(System.in);
+            String S = in.next();
+            int start = in.nextInt();
+            int end = in.nextInt();
 
-        Scanner scan = new Scanner(System.in);
-        String A = scan.next();
-        String B = scan.next();
-        int sum = A.length()+B.length();
-        System.out.println(sum);
-        if(A.length()<B.length())
-        {
-        System.out.println("Yes");
-        }
-        else   {System.out.println("No");}
+            if((S.length()<=100 && S.length()>=1)&&(start>=0 && start<end)&&(end<=S.length()) && S.matches("[a-zA-Z]+")){
 
-        String a = com.sun.xml.internal.ws.util.StringUtils.capitalize(A);
-        String b = com.sun.xml.internal.ws.util.StringUtils.capitalize(B);
-
-
-        System.out.println(a+" "+b);
+                System.out.println( S.substring(start, end));
+            }
+            else {System.out.println("Error input");}
     }
 
  }
